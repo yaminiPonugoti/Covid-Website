54d5932980a6955167d05092f2f7ee1d73d8ef4b
zwilk314 <zwilkerson314@gmail.com>
2020-07-20 11:54:18 -0400

    HTML-Javsacript interfacing work in "index.html," adding js file placeholders

---
+++ navFunctions.js
@@ -0,0 +1,12 @@
+//Website navigation scripts
+
+function loadNextPage(key){
+	switch(key){
+		case "survey1":
+			window.open();
+		case "survey2":
+			window.open();
+		case "adminLogin":
+			window.open();
+	}
+}
